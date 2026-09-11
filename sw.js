b5bac61cafaa915c7719981a95d57dcf57f1b5bf
romeoazhar <132354515+romeoazhar@users.noreply.github.com>
2026-09-12 03:51:06 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'maund-calc-v1';
+const FILES_TO_CACHE = ['index.html', 'manifest.json', 'icon.png'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES_TO_CACHE))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(res => res || fetch(e.request))
+  );
+});
